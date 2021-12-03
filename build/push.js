7d1622daff7d23252b6e4b54f5a19e6e7ec530fd
MOMO <momo@BJ203555.local>
2021-12-03 11:40:25 +0800

    feat:

--- build/push.js
+++ build/push.js
@@ -9,18 +9,23 @@ if (exec('git add .').code !== 0) {
   shell.exit(1);
 }
 if (exec(`git commit -am "${name}"`).code !== 0) {
-  echo('Error: Git commit failed');
+  echo('-e',"\033[0;33m Error: Git commit failed \033[0m");
   shell.exit(1);
 }
 if (exec('git push').code !== 0) {
   let _push = exec('git push')
   if(_push.code === 128){
     let newAfter = _push.stderr.match(/git.*/)
-    echo('-e',"\033[0;33m 新分支，请执行 \033[0m"+`${newAfter}`+"\033[0;33m 在远端建立新分支 \033[0m");
-    // 新分支是否执行
+    // echo('-e',"\033[0;33m 新分支，请执行 \033[0m"+`${newAfter}`+"\033[0;33m 在远端建立新分支 \033[0m");
+    echo('-e',"\033[0;33m 新分支，即将执行 \033[0m"+`${newAfter}`+"\033[0;33m 在远端建立新分支 \033[0m");
+    // 推送到新分支
+    if(exec(`git push --set-upstream origin ${newAfter}`).code !== 0){
+      echo('-e',"\033[0;33m Error: Git push failed \033[0m");
+      shell.exit(1);
+    }
     shell.exit(1);
   }else{
-    echo('Error: Git push failed');
+    echo('-e',"\033[0;33m Error: Git push failed \033[0m");
     shell.exit(1);
   }
 }
