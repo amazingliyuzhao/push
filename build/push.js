e8b3d2fe73dfe5c03d32a701c523125f94d501b8
MOMO <momo@BJ203555.local>
2021-12-03 11:46:28 +0800

    feat:testpush3

--- build/push.js
+++ build/push.js
@@ -16,9 +16,9 @@ if (exec('git push').code !== 0) {
   let _push = exec('git push')
   if(_push.code === 128){
     let newAfter = _push.stderr.match(/git.*/)
-    // echo('-e',"\033[0;33m 新分支，请执行 \033[0m"+`${newAfter}`+"\033[0;33m 在远端建立新分支 \033[0m");
     echo('-e',"\033[0;33m 新分支，即将执行 \033[0m"+`${newAfter}`+"\033[0;33m 在远端建立新分支 \033[0m");
     // 推送到新分支
+    console.log(exec(`git push --set-upstream origin ${newAfter}`))
     if(exec(`git push --set-upstream origin ${newAfter}`).code !== 0){
       echo('-e',"\033[0;33m Error: Git push failed \033[0m");
       shell.exit(1);
